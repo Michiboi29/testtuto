a5690ac75b26a13cceeecdca81e669bc24425d68
emich29 <emichaud29@gmail.com>
2020-03-13 18:02:17 -0400

    modif des fcts read et commande

--- Api/sql_fonctions.py
+++ Api/sql_fonctions.py
@@ -1,5 +1,6 @@
 from Api.connect import connect
 from Api.sql_columns import sql_columns
+from Api.checkexists import checkTableExists
 
 
 def sql_insert(table_name, **atributs):
@@ -13,8 +14,6 @@ def sql_insert(table_name, **atributs):
         if j != 'NULL':
             format_dict[i] = '%s'
 
-    print("atr_dict = ", atributs_dict)
-    print("form_dict = ", format_dict)
     atributs_str = ""
     format_val = ""
     for i in atributs_list:
@@ -27,26 +26,22 @@ def sql_insert(table_name, **atributs):
         if atributs_list.index(i) < len(atributs_list) - 1:
             atributs_str += ', '
 
-    print("val =", format_val)
-    print("atri_str= ", atributs_str)
-
     db = connect()
     curseur = db.cursor()
 
     sql = "INSERT INTO " + table_name + " (" + atributs_str + ") \
        VALUES (" + format_val + ")"
-    print(sql)
     sql = sql.format(**format_dict)
     valeurs = list(atributs.values())
     print(sql, valeurs)
+    try:
+        curseur.execute(sql, valeurs)
+        db.commit()
+        print(curseur.rowcount, "record inserted.")
 
-    curseur.execute(sql, valeurs)
-    db.commit()
-    print(curseur.rowcount, "record inserted.")
-
-    #except:
-      #  print("ereur")
-       # db.rollback()
+    except:
+        print("ereur d'insertion")
+        db.rollback()
 
     curseur.close()
     db.close()
@@ -60,6 +55,7 @@ def sql_delete(table_name, where):
 
     db = connect()
     curseur = db.cursor()
+
     curseur.execute(sql)
     db.commit()
     print(curseur.rowcount, "record(s) deleted")
@@ -69,21 +65,26 @@ def sql_delete(table_name, where):
 
 
 def sql_drop(table_name):
+
     db = connect()
     curseur = db.cursor()
     sql = "DROP TABLE IF EXISTS {}".format(table_name)
     curseur.execute(sql)
     curseur.close()
     db.close()
-    print('table ' + table_name + ' drop!')
+    if not checkTableExists(table_name):
+        print('table ' + table_name + ' drop!')
 
 
-def sql_update(table_name, set, valset, where):
+def sql_update(table_name, set, valset, where=None):
 
     sql = "UPDATE {0} \
-            SET {1} = %s \
-            WHERE {2}"
-    sql = sql.format(table_name, set, where)
+            SET {1} = %s"
+    sql = sql.format(table_name, set)
+
+    if where is not None:
+        where = " WHERE " + where
+    sql += where
 
     valset = [valset]
     print(sql, valset)
@@ -97,37 +98,36 @@ def sql_update(table_name, set, valset, where):
     curseur.close()
     db.close()
 
-    """cond =""
-    liste_where = list(where)
-    for key, value in where.items():
-        cond += str(key) + " = " + str(value)
-        if liste_where.index(key) < len(liste_where) - 1:
-            cond += " AND "
-    print("cond:", cond)
-    sql += cond
-    print(sql)"""
 
-
-def sql_read(table_name, distinct=False, where=None):
+def sql_read(table_name, select="*", distinct=False, where=None):
 
     dist = " "
     if distinct:
         dist = " DISTINCT "
 
-    sql = "SELECT{1}* FROM {0}".format(table_name, dist)
+    sql = "SELECT{1}{0} FROM {2}".format(select, dist, table_name)
 
     if where is not None:
         where = " WHERE " + where
-
-    sql += where
+        sql += where
 
     print("Reads:", sql)
+
+    colomns = sql_columns(table_name)
+    if select != "*":
+        colomns = []
+        sections = select.split()
+        sections.append("fin")
+        for col in sections:
+            if col == "fin":
+                break
+            if sections[(sections.index(col) + 1)] != "AS" and col != "AS":
+                col = col.replace(',', '')
+                colomns.append(col)
 
     db = connect()
     curseur = db.cursor()
 
-    colomns = sql_columns(table_name)
-    valeur = None
     result = []
     try:
 
@@ -135,7 +135,6 @@ def sql_read(table_name, distinct=False, where=None):
         valeur = curseur.fetchall()
         print(colomns)
         for row in valeur:
-            # Now print fetched result
             print(row)
             dictio = dict(zip(colomns, row))
             result.append(dictio)
@@ -151,33 +150,60 @@ def sql_read(table_name, distinct=False, where=None):
     return result
 
 
-def sql_comand(table_name, cmd):
+def sql_comand(cmd):
     db = connect()
     curseur = db.cursor()
-    print("Reads:", cmd)
+    print("Commande:", cmd)
 
-    colomns = sql_columns(table_name)
+    f = False
+    table_name = None
+    for i in cmd.split():
+        if f:
+            table_name = i
+        if i == "FROM":
+            f = True
 
-    valeur = None
-    result = []
-    try:
+    opperation = cmd.split()[0]
 
-        curseur.execute(cmd)
+    curseur.execute(cmd)
+    if opperation == "SELECT":
+        f = False
+        s = False
+        select = ""
+        for i in cmd.split():
+            if i == "FROM":
+                f = True
+            if s and not f:
+                select += " " + i
+            if i == "SELECT":
+                s = True
+
+        colomns = sql_columns(table_name)
+        sections = select.split()
+        if sections[0] != "*" and sections[0] != "DISTINCT":
+            colomns = []
+            sections.append("fin")
+            for col in sections:
+                if col == "fin":
+                    break
+                if sections[(sections.index(col) + 1)] != "AS" and col != "AS":
+                    col = col.replace(',', '')
+                    colomns.append(col)
+
+        result = []
         valeur = curseur.fetchall()
         print(colomns)
         for row in valeur:
-            # Now print fetched result
             print(row)
             dictio = dict(zip(colomns, row))
             result.append(dictio)
-
-    except:
-        print("Error: unable to fecth data")
-
-    print(valeur)
-    print(result)
-
+        print(result)
+        print(curseur.rowcount, "record(s) reached")
+    else:
+        result = None
+        db.commit()
+        print(curseur.rowcount, "record(s) affected")
     curseur.close()
     db.close()
 
-    return valeur
+    return result
--- apicoolaid.py
+++ apicoolaid.py
@@ -3,11 +3,8 @@ from Api.sql_fonctions import sql_delete, sql_comand, sql_drop, sql_update, sql_
 
 sql_drop('EMPLOYEE')
 sql_create()
-#sql_insert_ami(prenom='etienne', nom='mich', age=20)
-#sql_insert_ami(prenom='bob', sex='M')
-
 sql_insert('ami', prenom='guillaume', nom='laprise', age=20)
 sql_update('ami', 'prenom', "max", "nom = 'laprise' AND age < 30")
-sql_read('ami', True, "nom = 'mich'")
+sql_read(table_name='ami', select="prenom, ami.nom AS nom_famille", distinct=True)
 sql_delete('ami', 'nom IS NULL')
-sql_comand('ami', "SELECT DISTINCT * FROM ami")
+sql_comand("SELECT DISTINCT * FROM AMI")
